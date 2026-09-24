83a562f1c9cba83e9a09523b1b2a5eee612c5465
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-24 22:15:27 +0530

    chnaged in bob

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def display(name):
     if(name):
-        print("Hello from github to",name,"from bob branch side config.py")
+        print("Hello ",name,"from bob branch side config.py")
     else:
         print("No name found")
 name=input("Enter name: ")
