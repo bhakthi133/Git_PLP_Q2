f6d26745e5998ff630894d303c520aef9bdcd4ad
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-24 22:04:58 +0530

    Changed the line3

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def display(name):
     if(name):
-        print("Hello from github version control to",name,"from bob branch")
+        print("Hello from github version control to",name,"from bob branch side ")
     else:
         print("No name found")
 name=input("Enter name: ")
