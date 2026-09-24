c3ab33d6af8817bcf3186f2b36d4105d15cedbd1
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-24 21:51:27 +0530

    chnaged the file line on bob_branch

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def display(name):
     if(name):
-        print("Hello from github to",name)
+        print("Hello from github version control to",name)
     else:
         print("No name found")
 name=input("Enter name: ")
