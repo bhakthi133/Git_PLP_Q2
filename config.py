f0e59b4dd8f974d8c44c001a31e609d878cc8844
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-24 22:08:37 +0530

    Chnage in config in bob barnch

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def display(name):
     if(name):
-        print("Hello from github version control to",name,"from bob branch side ")
+        print("Hello from github to",name,"from bob branch side config.py")
     else:
         print("No name found")
 name=input("Enter name: ")
