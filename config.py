feb067212b060b5bb1555a59c4bd5e0bdef7586b
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-24 22:16:30 +0530

    Chnage in main

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def display(name):
     if(name):
-        print("Hello from github version control to",name,"From main")
+        print("Hello ",name,"From main")
     else:
         print("No name found")
 name=input("Enter name: ")
